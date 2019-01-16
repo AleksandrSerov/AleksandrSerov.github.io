4150f56b32a7f2632c3241218a49380bc8097793
Aleksandr Serov <sashaserov2010@gmail.com>
2019-01-16 09:32:17 +0300

    first commit

---
+++ precache-manifest.1c78fe7b9982fbe2a8b178bc322e2059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0e5f4581fdf679d7e7de",
+    "url": "/static/js/main.0e5f4581.chunk.js"
+  },
+  {
+    "revision": "c6be2199b9a1425b20ec",
+    "url": "/static/js/1.c6be2199.chunk.js"
+  },
+  {
+    "revision": "0e5f4581fdf679d7e7de",
+    "url": "/static/css/main.5a16c4d2.chunk.css"
+  },
+  {
+    "revision": "c6be2199b9a1425b20ec",
+    "url": "/static/css/1.67b750ec.chunk.css"
+  },
+  {
+    "revision": "17468a81695144f44fd6ff35f2077c6f",
+    "url": "/index.html"
+  }
+];
